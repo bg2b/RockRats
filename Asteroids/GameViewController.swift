ff31a22a2196a0278ebe8960475ab8552cce66e2
David Long <david@davidlong.org>
2019-09-14 05:14:45 -0400

    Note sure what supportedInterfaceOrientations was/is doing, but changed it to just landscape mode since that's all we support.

--- Asteroids/GameViewController.swift
+++ Asteroids/GameViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-  weak var scene: GameScene? = nil
+  weak var scene: BasicScene? = nil
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -44,11 +44,7 @@ class GameViewController: UIViewController {
   }
 
   override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-    if UIDevice.current.userInterfaceIdiom == .phone {
-      return .allButUpsideDown
-    } else {
-      return .all
-    }
+    return .landscape
   }
 
   override var prefersStatusBarHidden: Bool {
