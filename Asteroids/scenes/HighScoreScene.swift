d7cc72c04695f70510a1f9bd772053103195d30e
David Long <david@davidlong.org>
2019-11-23 19:26:09 -0500

    Add documentation for HighScoreScene

--- Asteroids/scenes/HighScoreScene.swift
+++ Asteroids/scenes/HighScoreScene.swift
@@ -9,6 +9,9 @@
 import SpriteKit
 import GameKit
 
+/// Ensure that a name that won't make the high scores list too wide
+/// - Parameter playerName: The name to be display
+/// - Returns: The name unchanged if it's short, otherwise an abbreviated name
 func abbreviatedName(_ playerName: String) -> String {
   let maxLength = 20
   if playerName.count > maxLength {
@@ -19,12 +22,28 @@ func abbreviatedName(_ playerName: String) -> String {
   }
 }
 
+/// The scene for displaying a list of high scores
+///
+/// This is automatically shown at the end of a game, and can also be invoked from
+/// the main menu.
 class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
-  var scores: SKNode!
+  /// The button that invokes Game Center, if that's enabled
   var gcButton: Button!
+  /// This is `true` when I'm presenting the view controller supplied by Game Center
+  /// with achievements and high scores
   var showingGCVC = false
-  var newGame: GameScene? = nil
 
+  /// Make the labels for a line in the high scores display
+  ///
+  /// There are two labels per line, one for the player name and one for the score.
+  /// The `highlighted` score is typically the score from the just-played game.  If
+  /// `highScore` has the same player, then the name will be highlighted.  If
+  /// `highScore` is also the same score, then the points will be highlighted too.
+  ///
+  /// - Parameters:
+  ///   - highScore: The score to generate labels for
+  ///   - highlighted: A score that should be highlighted
+  /// - Returns: A tuple of labels, player name and score
   func highScoreLineLabels(_ highScore: GameScore, highlighted: GameScore) -> (SKLabelNode, SKLabelNode) {
     let playerScore = SKLabelNode()
     playerScore.name = "playerScore"
@@ -50,6 +69,16 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     return (playerName, playerScore)
   }
 
+  /// Construct an `SKNode` for the high score list
+  ///
+  /// The `highlighted` score is for the just-played game.  If that score isn't in
+  /// the list, then there's an extra line added at the end of the high scores for
+  /// `highlighted`.
+  ///
+  /// - Parameters:
+  ///   - highScores: The list of high scores
+  ///   - highlighted: What to highlight (see `highScoreLineLabels`)
+  /// - Returns: A node hierarchy that corresponds to the high scores display
   func highScoreLines(_ highScores: [GameScore], highlighted: GameScore) -> SKNode {
     let scores = SKNode()
     scores.name = "highScoreLines"
@@ -71,6 +100,9 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     let height = labels[0].0.frame.height + 2 * paddingY
     var nextY = CGFloat(0)
     for (i, (playerName, playerScore)) in labels.enumerated() {
+      // Each line in the display consists of a left-justified name, a
+      // right-justified score, and a faint box around the two so that the eye can
+      // match up name to score more easily.
       let line = SKNode()
       line.name = "highScoreLine"
       if i == numHighScores {
@@ -95,12 +127,17 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     return scores
   }
 
+  /// Construct all the information on the high scores scene
+  /// - Parameters:
+  ///   - score: The high scores
+  ///   - highScores: What to highlight (typically the score of the just-played game)
   func initScores(score: GameScore?, highScores: [GameScore]) {
     let highlighted = score ?? GameScore(points: 0)
-    scores = SKNode()
+    let scores = SKNode()
     scores.name = "scores"
     scores.setZ(.info)
     addChild(scores)
+    // Title
     let title = SKLabelNode(fontNamed: AppColors.font)
     title.fontSize = 100
     title.fontColor = AppColors.highlightTextColor
@@ -108,6 +145,7 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     title.verticalAlignmentMode = .center
     title.position = CGPoint(x: fullFrame.midX, y: fullFrame.maxY - title.fontSize)
     scores.addChild(title)
+    // Buttons at the bottom
     let buttonSize = CGSize(width: 150, height: 100)
     let buttonSpacing = CGFloat(20)
     let buttonY = fullFrame.minY + buttonSize.height + buttonSpacing
@@ -122,11 +160,15 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     gcButton = Button(imageNamed: "gamecenterbutton", imageColor: .white, size: buttonSize)
     gcButton.position = CGPoint(x: playButton.position.x + buttonSize.width + buttonSpacing, y: playButton.position.y)
     gcButton.action = { [unowned self] in self.showGameCenter() }
+    // The Game Center button might need to be disabled
     if !Globals.gcInterface.enabled {
       gcButton.disable()
     }
+    // If the Game Center state changes, I have to change the button's
+    // enabled/disabled state too.
     NotificationCenter.default.addObserver(self, selector: #selector(gcStateChanged), name: .authenticationChanged, object: nil)
     scores.addChild(gcButton)
+    // The actual high scores
     let highScores = highScoreLines(highScores, highlighted: highlighted)
     let wantedMidY = 0.5 * (title.frame.minY + playButton.calculateAccumulatedFrame().maxY)
     // Center highScores vertically at wantedMidY
@@ -136,14 +178,19 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     scores.addChild(highScores)
   }
 
+  /// Start a new game
   func startGame() {
     switchToScene { return GameScene(size: self.fullFrame.size) }
   }
 
+  /// Switch back to the main menu
   func mainMenu() {
     showWhenQuiescent(Globals.menuScene)
   }
 
+  /// The Game Center status has changed, so update `gcButton`'s enabled/disabled
+  /// state to match
+  /// - Parameter notification: A notification indicating what happened
   @objc func gcStateChanged(_ notification: Notification) {
     logging("High score scene got notification of Game Center state change")
     if notification.object as? Bool ?? false {
@@ -156,6 +203,7 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
   /// Enforce pausing when showing the Game Center view controller.
   override var forcePause: Bool { showingGCVC }
 
+  /// Display the view controller from Game Center with leaderboards and achievements
   func showGameCenter() {
     guard let rootVC = view?.window?.rootViewController, Globals.gcInterface.enabled else {
       logging("Can't show Game Center")
@@ -163,6 +211,8 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     }
     let gcvc = GKGameCenterViewController()
     gcvc.gameCenterDelegate = self
+    // The high scores scene includes information from the Game Center leaderboards
+    // already, so default to showing the player's achievements.
     gcvc.viewState = .achievements
     gcvc.leaderboardTimeScope = .week
     isPaused = true
@@ -170,6 +220,8 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     rootVC.present(gcvc, animated: true)
   }
 
+  /// Called when the Game Center view controller should be dismissed
+  /// - Parameter gcvc: The Game Center view controller
   func gameCenterViewControllerDidFinish(_ gcvc: GKGameCenterViewController) {
     gcvc.dismiss(animated: true) {
       self.showingGCVC = false
@@ -177,15 +229,25 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     }
   }
 
+  /// Not much to see here
+  /// - Parameter view: The view that will present the scene
   override func didMove(to view: SKView) {
     super.didMove(to: view)
     logging("\(name!) finished didMove to view")
   }
 
+  /// Not much to see here (yet)
+  ///
+  /// - Todo: Add some animations to the high score scene
+  ///
+  /// - Parameter currentTime: The current game time
   override func update(_ currentTime: TimeInterval) {
     super.update(currentTime)
   }
 
+  /// Construct a high score display scene; don't call this, but use the convenience
+  /// init below.
+  /// - Parameter size: The size of the scene
   override init(size: CGSize) {
     super.init(size: size)
     name = "highScoreScene"
@@ -197,9 +259,17 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
     fatalError("init(coder:) has not been implemented by HighScoreScene")
   }
 
+  /// Construct a new high score scene
+  /// - Parameters:
+  ///   - size: The size of the scene
+  ///   - score: The score of the just-played game (or `nil` if the previous scene
+  ///     was the main menu)
   convenience init(size: CGSize, score: GameScore?) {
+    // Construct the scene's skeleton
     self.init(size: size)
+    // Start with the local high scores
     var highScores = userDefaults.highScores.value
+    // Merge in scores from Game Center, if those are available
     if let gc = Globals.gcInterface, gc.enabled, !gc.leaderboardScores.isEmpty {
       var ranks = Array(1 ... gc.leaderboardScores.count)
       if gc.leaderboardScores.count >= 4 {
@@ -230,12 +300,17 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
           highScores.append(globalScore)
         }
       }
+      // Sort everything by points
       highScores = highScores.sorted { $0.points > $1.points || ($0.points == $1.points && $0.date > $1.date) }
       for score in highScores {
         let date = Date(timeIntervalSinceReferenceDate: score.date)
         print("\(score.playerName ?? "unknown") \(score.points) \(score.date) \(date)")
       }
       var playerScore = score?.points ?? 0
+      // See if the player has achieved a high rank on the weekly leaderboards
+      // (either through the just-played game, or through a previous game that they
+      // played sometime during the last week that is now a top score because some
+      // other scores dropped off by being too old).
       if let previousWeeklyPlayerScore = gc.localPlayerScore {
         playerScore = max(playerScore, GameScore(score: previousWeeklyPlayerScore).points)
       }
@@ -251,6 +326,7 @@ class HighScoreScene: BasicScene, GKGameCenterControllerDelegate {
         reportAchievement(achievement: .top10)
       }
     }
+    // Display the top ten combined scores
     initScores(score: score, highScores: Array(highScores.prefix(10)))
   }
 }
