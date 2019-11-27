55fc379791e0cc2b65915d8ae5051c26cfcf5e6c
David Long <david@davidlong.org>
2019-11-27 09:33:29 -0500

    Documentation for SpriteCache.swift

--- Asteroids/SpriteCache.swift
+++ Asteroids/SpriteCache.swift
@@ -8,12 +8,23 @@
 
 import SpriteKit
 
+/// A cache for sprites that get created and destroyed a lot, e.g., asteroids of
+/// various shapes and shots.  Sprites managed by this class must not be renamed,
+/// since the name is used as a key to identify the type of sprite.
 class SpriteCache {
+  /// A dictionary mapping image names to a list of available sprites
   var sprites = [String: [SKSpriteNode]]()
+  /// Number of sprites created
   var created = 0
+  /// Number of sprites used
   var used = 0
+  /// Number of sprites in the recycling bin waiting for reuse
   var recycled = 0
 
+  /// Create a new sprite
+  /// - Parameters:
+  ///   - name: The image name for the sprite's texture
+  ///   - initializer: An optional closure to initialize the new sprite
   func makeSprite(imageNamed name: String, initializer: ((SKSpriteNode) -> Void)?) -> SKSpriteNode {
     let sprite = SKSpriteNode(texture: Globals.textureCache.findTexture(imageNamed: name))
     sprite.name = name
@@ -22,39 +33,50 @@ class SpriteCache {
     return sprite
   }
 
+  /// Find an existing unused sprite, or make a new one
+  /// - Parameters:
+  ///   - name: The image name for the sprite's texture
+  ///   - initializer: An optional closure to initialize the sprite if it has to be created
   func findSprite(imageNamed name: String, initializer: ((SKSpriteNode) -> Void)? = nil) -> SKSpriteNode {
     used += 1
     if sprites[name] == nil {
+      // First time asking for this particular type of sprite
       sprites[name] = [SKSpriteNode]()
     }
     if sprites[name]!.isEmpty {
+      // The recycle bin is empty, make a new sprite
       return makeSprite(imageNamed: name, initializer: initializer)
     }
     recycled -= 1
+    // Reuse an item from the bin
     return sprites[name]!.popLast()!
   }
 
+  /// Recycle a sprite that's no longer needed so that it can be reused later
+  /// - Parameter sprite: The sprite to recycle
   func recycleSprite(_ sprite: SKSpriteNode) {
-    if let _ = sprite.parent {
-      sprite.removeFromParent()
-    }
+    // Make sure the sprite is in a consistent state
+    sprite.removeFromParent()
+    sprite.removeAllActions()
     if let body = sprite.physicsBody {
       body.velocity = .zero
       body.angularVelocity = 0
     }
-    sprite.removeAllActions()
     sprite.position = .zero
     sprite.speed = 1
     sprite.isPaused = false
+    // Add the sprite to the recycling bin
     sprites[sprite.name!]!.append(sprite)
     recycled += 1
   }
 
+  /// Print some random statistics
   func stats() {
     logging("Sprite cache created \(created) unique sprites, used \(used); \(recycled) sprites are in the recycle bin")
   }
 }
 
 extension Globals {
+  /// A cache for all the simple sprites in the game
   static let spriteCache = SpriteCache()
 }
