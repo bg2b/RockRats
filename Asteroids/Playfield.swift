d4f43624eecbf8c959906ec2fbe01184d340b497
David Long <david@davidlong.org>
2019-11-26 21:56:28 -0500

    Document Playfield.swift

--- Asteroids/Playfield.swift
+++ Asteroids/Playfield.swift
@@ -8,9 +8,19 @@
 
 import SpriteKit
 
+/// The game area, where all the action takes place
+///
+/// This holds asteroids, the player's ship, UFOs, explosions, etc.  It's also
+/// rsponsible for wrapping things from one side of the screen to the other, and for
+/// keeping track of the status flags (stored in the category bitmask) indicating
+/// whether something has been visible on the screen and whether it has wrapped.
 class Playfield: SKNode {
+  /// The bounds of the play area; center is (0, 0), positive x to the right,
+  /// positive y up
   var bounds: CGRect
 
+  /// Create the playfield
+  /// - Parameter bounds: The desired bounds
   required init(bounds: CGRect) {
     self.bounds = bounds
     super.init()
@@ -21,26 +31,35 @@ class Playfield: SKNode {
     fatalError("init(coder:) has not been implemented by Playfield")
   }
 
+  /// Add something to the playfield
+  /// - Parameter child: The node to add
   func addWithScaling(_ child: SKNode) {
     addChild(child)
+    // Things without a physics body are effects and don't need anything special
     guard let body = child.physicsBody else { return }
+    // Something just being added to the playfield has not wrapped around
     body.hasWrapped = false
+    // If the current simulation speed is 1 (normal) or the body is not a fragment
+    // from an explosion, don't do anything special
     guard speed != 1, body.isA(.fragment) else { return }
-    // Fragments are not affected by slow-motion
+    // A fragment is being added to the playfield, and the simulation is running in
+    // slow-motion.  Fragments are not affected by slow-motion, so the velocity,
+    // angular velocity, and action speed of the fragment all have to be adjusted to
+    // counteract the slow down.
     body.velocity = body.velocity.scale(by: 1 / speed)
     body.angularVelocity /= speed
     child.speed /= speed
   }
-  
-  func setBounds(to newBounds: CGRect) {
-    bounds = newBounds
-  }
 
+  /// Change the speed of the simulation (either start a slow-motion effect or go
+  /// back to normal speed)
+  /// - Parameter newSpeed: The desired speed (0.5 would be a 2x slowdown)
   func changeSpeed(to newSpeed: CGFloat) {
     guard let physics = scene?.physicsWorld else { return }
+    // Scan to find fragments; they have to be adjusted for the changing speed
     for p in children {
       if let body = p.physicsBody, body.isA(.fragment) {
-        // Change any existing fragments to compensate for the change in speed
+        // Update the fragment's velocity, angular velocity, and action speed
         body.velocity = body.velocity.scale(by: speed / newSpeed)
         body.angularVelocity *= speed / newSpeed
         p.speed /= newSpeed
@@ -54,22 +73,29 @@ class Playfield: SKNode {
     physics.speed = newSpeed
   }
 
+  /// Is anything interesting happening in the playfield?
+  /// - Parameter transient: A category bitmask (constructed using `setOf`) that
+  ///   specifies things that should have disappeared in order for the playfield to
+  ///   be quiescent
   func isQuiescent(transient: UInt32) -> Bool {
     for child in children {
-      // Anything without a physicsBody is some sort of effect that's running.
+      // Anything without a physicsBody is some sort of effect that's running, so if
+      // there are any of those then the field is not quiescent
       guard let body = child.physicsBody else { return false }
       if body.isOneOf(transient) {
-        // Fragments, shots, whatever must finish their lives before we're quiescent.
+        // There's still something transient like a fragment or a warping out UFO, so
+        // not quiescent
         return false
       }
     }
     return true
   }
 
+  /// Recycle anything that should go in the global sprite cache
+  ///
+  /// This gets called when a scene in a perhaps uncertain state should be prepared
+  /// for garbage collection.
   func recycle() {
-    // Recycle anything that should go in the global sprite cache.  This gets called
-    // when a scene in a perhaps uncertain state should be prepared for garbage
-    // collection.
     let recycleable = setOf([ObjectCategories.playerShot, .ufoShot, .asteroid])
     for child in children {
       guard let body = child.physicsBody else { continue }
@@ -79,18 +105,20 @@ class Playfield: SKNode {
     }
   }
 
+  /// Wrap the coordinates of all the objects in the playfield
   func wrapCoordinates() {
     for child in children {
+      // Don't move anything that doesn't have a physics body
       guard let body = child.physicsBody else { continue }
       if !body.isOnScreen {
-        // This isn't on screen yet, so we're just waiting for it to appear before we
-        // start wrapping.
+        // This isn't on screen yet, so wait for it to appear before possibly
+        // wrapping
         if bounds.contains(child.position) {
           body.isOnScreen = true
         }
       } else {
-        // We wrap only after going past the edge a little bit so that an object
-        // that's moving just along the edge won't stutter back and forth.
+        // Wrap only after something went past the edge a little bit.  That way an
+        // object that's moving just along the edge won't stutter back and forth.
         let hysteresis = CGFloat(3)
         if child.position.x < bounds.minX - hysteresis {
           child.position.x += bounds.width
