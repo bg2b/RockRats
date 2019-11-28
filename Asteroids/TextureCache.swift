f42925d3b4d85179aae2ebe53e16b72bad236a3a
David Long <david@davidlong.org>
2019-11-27 19:28:50 -0500

    Document texture cache

--- Asteroids/TextureCache.swift
+++ Asteroids/TextureCache.swift
@@ -8,29 +8,39 @@
 
 import SpriteKit
 
+/// A cache of, yes, you guessed it, textures!
 class TextureCache {
-  var used = 0
+  /// A dictionary mapping texture names to loaded textures
   var textures = [String: SKTexture]()
+  /// The number of textures returned from the cache
+  var used = 0
 
+  /// Get the texture for an image name
+  /// - Parameter name: The image name
+  /// - Returns: The texture
   func findTexture(imageNamed name: String) -> SKTexture {
     used += 1
     if let cached = textures[name] {
+      // Already loaded, just reuse
       return cached
     } else {
       let texture = SKTexture(imageNamed: name)
-      // It seems like the texture is not always really loaded for reasons that I don't
-      // understand.  Evaluating the size seems to force it to do the loading for real.
-      let _ = texture.size()
+      // Textures sometimes don't seem to be really loaded unless I force the issue
+      // by calculating the size...
+      _ = texture.size()
+      // Save it for reuse
       textures[name] = texture
       return texture
     }
   }
 
+  /// Print some random statistics
   func stats() {
     print("Texture cache loaded \(textures.count) textures, used \(used)")
   }
 }
 
 extension Globals {
+  /// A cache holding the common textures used in the app
   static let textureCache = TextureCache()
 }
