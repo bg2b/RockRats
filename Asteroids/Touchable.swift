d3822bea5438d981e364de1d19047653c5f8c6eb
David Long <david@davidlong.org>
2019-12-01 15:09:45 -0500

    Document Touchable

--- Asteroids/Touchable.swift
+++ Asteroids/Touchable.swift
@@ -10,8 +10,13 @@ import SpriteKit
 
 /// A wrapper around a node that makes it respond to touches
 class Touchable: SKNode {
+  /// The action to perform upon touch
   let action: () -> Void
 
+  /// Create a touch wrapper node
+  /// - Parameters:
+  ///   - child: The node to make respond to touches
+  ///   - action: The action to perform upon touch
   init(_ child: SKNode, _ action: @escaping () -> Void) {
     self.action = action
     super.init()
@@ -23,11 +28,16 @@ class Touchable: SKNode {
     fatalError("init(coder:) has not been implemented by Touchable")
   }
 
+  /// Make the wrapper handle touches
   override var isUserInteractionEnabled: Bool {
     get { return true }
     set {}
   }
 
+  /// Perform an action when touched
+  /// - Parameters:
+  ///   - touches: Some random touches
+  ///   - event: The event the touches belong to
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     action()
   }
